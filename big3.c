8b3b9094dc2644f511f05799f7f329110e4c7ff7
vreddyjld <87608248+vreddyjld@users.noreply.github.com>
2021-07-19 01:08:30 +0530

    Update big3.c

--- big3.c
+++ big3.c
@@ -4,7 +4,7 @@ void biggest3() {
     printf("Enter three different numbers: ");
     scanf("%lf %lf %lf", &n1, &n2, &n3);
 
-    // if n1 is greater than both n2 and n3, n1 is the largest
+    // if n1 is greater than both n2 and n3, n1 is largest
     if (n1 >= n2 && n1 >= n3)
         printf("%.2f is the largest number.", n1);
 
